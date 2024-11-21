7d30ec5f5a6e18ee60c8b790b8349fde54945a3b
patilsg <patilsg@LCBLAB2110-10>
2024-11-21 11:22:43 -0500

    ok so love it

--- InClass2024-IS3050-001/HelloWorld.cs
+++ InClass2024-IS3050-001/HelloWorld.cs
@@ -7,5 +7,9 @@ namespace InClass2024_IS3050_001
 {
     public class HelloWorld
     {
+        public static void SayHello()
+        {
+            Console.WriteLine("Hello from HelloWorld.SayHello()");
+        }
     }
 }
